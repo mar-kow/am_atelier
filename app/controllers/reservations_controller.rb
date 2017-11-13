9887b5320496c842a31c4698dca44c7eeae04f59
Adam <adam.majkowski93@gmail.com>
2017-11-13 19:46:10 +0100

    Add more email configuration

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -8,6 +8,7 @@ class ReservationsController < ApplicationController
 
   def take
     reservations_handler.take(book)
+    UserMailer.confirm_email(User.find(current_user.id), current_user.email).deliver_now
     redirect_to(book_path(book.id))
   end
 
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,8 +2,10 @@ class UserMailer < ActionMailer::Base
   default from: 'warsztaty@infakt.pl'
   layout 'mailer'
 
-  def confirm_email(user)
+  def confirm_email(user, email)
     @user = user
+
+    mail(to: email, subject: "Potwierdź adres email")
   end
 
   def unsubscribe
