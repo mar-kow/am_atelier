95517eb8a4e63a2171d8aa9836d08548e0e50b8b
Marta Kowalska <marta.kowalska.987@gmail.com>
2017-11-22 20:46:42 +0100

    add info about omniauth in user model - devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         :omniauthable, omniauth_providers: [:google_oauth2]
 
   has_many :reservations
 end
