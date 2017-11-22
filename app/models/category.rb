990d5de2929f007999c42cd60125d829bf4c6854
Marta Kowalska <marta.kowalska.987@gmail.com>
2017-11-22 19:42:49 +0100

    add #similar_books and #similar_ids to category

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,4 +4,14 @@ class Category < ActiveYaml::Base
   def books
     ::Book.where(category_id: self.id)
   end
+
+  def similar_books
+    ::Book.where(category_id: similar_ids)
+  end
+
+  private
+
+  def similar_ids
+    similar_categories.map { |name| Category.find_by(name: name).id }
+  end
 end
